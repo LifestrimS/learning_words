7cdba39838191233f7fab9db388bde0102259c1e
LifestrimS <LifestrimStrife@gmail.com>
2024-07-14 16:57:00 +0300

    mainScreen draft

---
+++ lib/mainScreen/game_widget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:learning_words/utils.dart/colors.dart';
+
+class GameWidget extends StatelessWidget {
+  final String name;
+  final VoidCallback onTap;
+  const GameWidget({required this.name, required this.onTap, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 69.0,
+        clipBehavior: Clip.antiAlias,
+        margin: const EdgeInsets.only(bottom: 18.0),
+        decoration: BoxDecoration(
+            border: Border.all(color: AppColors.black, width: 1.0),
+            borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(16.0),
+                bottomLeft: Radius.circular(16.0))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(left: 20.0, top: 14.0, bottom: 14.0),
+              child: Text(
+                name,
+                style: const TextStyle(
+                    color: AppColors.black, fontSize: 32.0, height: 0.0),
+              ),
+            ),
+            Container(
+              width: 67.0,
+              height: double.infinity,
+              clipBehavior: Clip.antiAlias,
+              decoration: const BoxDecoration(
+                color: AppColors.black,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(15.0),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: RotatedBox(
+                  quarterTurns: 1,
+                  child: SvgPicture.asset(
+                    'assets/icons/triangle.svg',
+                    colorFilter: const ColorFilter.mode(
+                        AppColors.white, BlendMode.srcIn),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils.dart/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF1A1A1A);
+  static const Color grey = Color(0xFFD9D9D9);
+
+  //Shadows
+  static BoxShadow containerShadow = BoxShadow(
+    color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.5),
+    blurRadius: 4.0,
+    offset: const Offset(4.0, 4.0),
+  );
+
+  static BoxShadow appBarShadow = BoxShadow(
+    color: const Color.fromARGB(255, 158, 158, 158).withOpacity(0.5),
+    blurRadius: 4.0,
+    offset: const Offset(1.0, 4.0),
+  );
+}
