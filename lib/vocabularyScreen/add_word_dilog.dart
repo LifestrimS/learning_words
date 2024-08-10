9d5c3229c654f6b3571a1eda03c56b960b9b6d28
LifestrimS <LifestrimStrife@gmail.com>
2024-08-10 19:03:01 +0300

    diolog fixes

--- lib/vocabularyScreen/add_word_dilog.dart
+++ lib/vocabularyScreen/add_word_dilog.dart
@@ -7,7 +7,7 @@ import 'package:learning_words/repository/repository.dart';
 import 'package:learning_words/utils.dart/colors.dart';
 
 class AddWordDilog {
-  final Function(bool value) onConfirm; // <------------|
+  final Function(bool value) onConfirm;
 
   AddWordDilog({
     required this.onConfirm,
@@ -27,65 +27,71 @@ class AddWordDilog {
       context: context,
       builder: (context) {
         return Dialog(
-          child: Container(
-            padding: const EdgeInsets.all(20.0),
-            decoration: const BoxDecoration(
-              color: AppColors.black,
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(16.0),
-                bottomLeft: Radius.circular(16.0),
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              decoration: const BoxDecoration(
+                color: AppColors.black,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(16.0),
+                  bottomLeft: Radius.circular(16.0),
+                ),
               ),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                textField(
-                    controller: _textFieldWordController,
-                    hintText: 'Word',
-                    height: 50.0),
-                textField(
-                    controller: _textFieldTranscriptionController,
-                    hintText: 'Transcription',
-                    height: 50.0),
-                textField(
-                    controller: _textFieldTranslationController,
-                    hintText: 'Translation',
-                    height: 100.0),
-                textField(
-                    controller: _textFieldExampleController,
-                    hintText: 'Examples',
-                    height: 200.0),
-                GestureDetector(
-                  onTap: () {
-                    onSave().then((value) {
-                      if (value) {
-                        onConfirm(true);
-                        Navigator.pop(context);
-                      }
-                    });
-                  },
-                  child: Container(
-                    width: double.infinity,
-                    height: 50.0,
-                    decoration: const BoxDecoration(
-                      color: AppColors.white,
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(8.0),
-                        bottomLeft: Radius.circular(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  textField(
+                      controller: _textFieldWordController,
+                      hintText: 'Word',
+                      height: 50.0,
+                      maxLines: 1),
+                  textField(
+                      controller: _textFieldTranscriptionController,
+                      hintText: 'Transcription',
+                      height: 50.0,
+                      maxLines: 1),
+                  textField(
+                      controller: _textFieldTranslationController,
+                      hintText: 'Translation',
+                      height: 100.0,
+                      maxLines: 3),
+                  textField(
+                      controller: _textFieldExampleController,
+                      hintText: 'Examples',
+                      height: 200.0,
+                      maxLines: 6),
+                  GestureDetector(
+                    onTap: () {
+                      onSave().then((value) {
+                        if (value) {
+                          onConfirm(true);
+                          Navigator.pop(context);
+                        }
+                      });
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      height: 50.0,
+                      decoration: const BoxDecoration(
+                        color: AppColors.white,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(8.0),
+                          bottomLeft: Radius.circular(8.0),
+                        ),
+                      ),
+                      margin: const EdgeInsets.only(top: 60.0 - 12.0),
+                      child: const Center(
+                        child: Text(
+                          'Save',
+                          textAlign: TextAlign.center,
+                          style:
+                              TextStyle(color: AppColors.black, fontSize: 24.0),
+                        ),
                       ),
                     ),
-                    margin: const EdgeInsets.only(top: 60.0 - 12.0),
-                    child: const Center(
-                      child: Text(
-                        'Save',
-                        textAlign: TextAlign.center,
-                        style:
-                            TextStyle(color: AppColors.black, fontSize: 24.0),
-                      ),
-                    ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
           ),
         );
@@ -93,34 +99,31 @@ class AddWordDilog {
     );
   }
 
-  //TODO: notCentered text
-  //TODO: keboard overflow container
-
   Widget textField(
       {required TextEditingController controller,
       required String hintText,
-      required double height}) {
-    return Container(
-      height: height,
-      padding: const EdgeInsets.symmetric(horizontal: 3.0, vertical: 6.0),
-      margin: const EdgeInsets.only(bottom: 12.0),
-      decoration: const BoxDecoration(
-        color: AppColors.white,
-        borderRadius: BorderRadius.only(
-          topRight: Radius.circular(8.0),
-          bottomLeft: Radius.circular(8.0),
-        ),
-      ),
+      required double height,
+      required int maxLines}) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12.0),
       child: TextFormField(
         controller: controller,
         textAlignVertical: TextAlignVertical.top,
+        maxLines: maxLines,
         style: const TextStyle(color: AppColors.black, fontSize: 20.0),
         decoration: InputDecoration(
-          contentPadding: EdgeInsets.zero,
-          hintText: hintText,
-          border: InputBorder.none,
-          hintStyle: const TextStyle(color: AppColors.black),
-        ),
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 3.0, vertical: 6.0),
+            hintText: hintText,
+            border: const OutlineInputBorder(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(8.0),
+                bottomLeft: Radius.circular(8.0),
+              ),
+            ),
+            hintStyle: const TextStyle(color: AppColors.black),
+            fillColor: AppColors.white,
+            filled: true),
       ),
     );
   }
